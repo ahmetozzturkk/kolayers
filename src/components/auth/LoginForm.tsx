699e1ee145c745da66e2195c03debaffecb76d2e
ahmetozzturkk <ahmet_ozturk92@hotmail.com>
2025-05-13 17:25:13 +0300

    Fix import path for authService in LoginForm

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { login } from '@/services/authService';
+import { login } from '@/src/services/authService';
 import { useRouter } from 'next/navigation';
 
 export default function LoginForm() {
